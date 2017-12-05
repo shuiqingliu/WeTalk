b61a45f07cb018f5ab58c7a5fa81a7baff7f44a4
qingliu <shuiqingliu14@gmail.com>
2017-12-05 23:38:12 +0800

    add:添加聊天页面，方便其他模块调用

---
+++ TLChat/Modules/Chat/ChatViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChatViewController.h
+//  TLChat
+//
+//  Created by qingliu on 12/5/17.
+//  Copyright © 2017 李伯坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TLTableViewController.h"
+
+@interface ChatViewController : TLTableViewController
+
+@property (nonatomic,copy) NSString *chatName;
+
+@end
---
+++ TLChat/Modules/Chat/ChatViewController.m
@@ -0,0 +1,61 @@
+//
+//  ChatViewController.m
+//  TLChat
+//
+//  Created by qingliu on 12/5/17.
+//  Copyright © 2017 李伯坤. All rights reserved.
+//
+
+#import "ChatViewController.h"
+
+@interface ChatViewController ()
+
+@property (nonatomic,strong) UIImageView *chatScrollImageView;
+
+@end
+
+@implementation ChatViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.navigationItem setTitle:self.chatName];
+    [self initChatUI];
+
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - private method
+
+- (void)initChatUI{
+    [self.tableView setBackgroundColor:[UIColor whiteColor]];
+    [self.tabBarItem setTitle:self.chatName];
+    // [self.tableView setTableHeaderView:self.searchController.searchBar];
+    [self.tableView addSubview:self.chatScrollImageView];
+    [self.chatScrollImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.tableView);
+        make.bottom.mas_equalTo(self.tableView.mas_top).mas_offset(-35);
+    }];
+    
+//    UIBarButtonItem *rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"nav_add"] style:UIBarButtonItemStyleDone target:self action:@selector(rightBarButtonDown:)];
+//    [self.navigationItem setRightBarButtonItem:rightBarButtonItem];
+}
+
+//UIImageView setter method
+- (UIImageView*)chatScrollImageView{
+    if(_chatScrollImageView == nil){
+        _chatScrollImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"conv_wechat_icon"]];
+    }
+    return _chatScrollImageView;
+}
+
+
+// TODO: add bottom toolbar to input text, record message and send image
+
+@end
+
